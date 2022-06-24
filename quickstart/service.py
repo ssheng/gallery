59dfe71f5c031b19ad57b2a40a50191e066175af
Sean Sheng <s3sheng@gmail.com>
2022-06-24 15:56:02 -0700

    Transformers custom pipelines

--- quickstart/service.py
+++ quickstart/service.py
@@ -1,9 +1,30 @@
 import bentoml
+
 from bentoml.io import Text, JSON
+from transformers.pipelines.fill_mask import FillMaskPipeline
 
 runner = bentoml.transformers.get("fill-mask:x73hzqhqqk3bwcvj").to_runner()
 
 svc = bentoml.Service("fill-mask", runners=[runner])
+
+class MyPipeline(FillMaskPipeline):
+    def _sanitize_parameters(self, **kwargs):
+        preprocess_kwargs = {}
+        if "maybe_arg" in kwargs:
+            preprocess_kwargs["maybe_arg"] = kwargs["maybe_arg"]
+        return preprocess_kwargs, {}, {}
+
+    def preprocess(self, inputs, maybe_arg=2):
+        text = inputs['text']
+        input_ids = self.tokenizer(text, return_tensors='pt')
+        return input_ids
+
+    def _forward(self, model_inputs):
+        outputs = self.model(**model_inputs)
+        return outputs
+
+    def postprocess(self, model_outputs):
+        return model_outputs.logits.softmax(1)[0, 1].item()
 
 @svc.api(input=Text(), output=JSON())
 def classify(input_series: str) -> dict:
--- quickstart/train.py
+++ quickstart/train.py
@@ -1,15 +1,47 @@
 import bentoml
 import transformers
-
 import logging
+
+from transformers import AutoTokenizer, AutoModelForCausalLM, Pipeline
+from transformers.pipelines import SUPPORTED_TASKS
 
 logging.basicConfig(level=logging.WARN)
 
+class MyPipeline(Pipeline):
+    def _sanitize_parameters(self, **kwargs):
+        preprocess_kwargs = {}
+        if "maybe_arg" in kwargs:
+            preprocess_kwargs["maybe_arg"] = kwargs["maybe_arg"]
+        return preprocess_kwargs, {}, {}
+
+    def preprocess(self, text, maybe_arg=2):
+        input_ids = self.tokenizer(text, return_tensors='pt')
+        return input_ids
+
+    def _forward(self, model_inputs):
+        outputs = self.model(**model_inputs)
+        return outputs
+
+    def postprocess(self, model_outputs):
+        return model_outputs
+
 if __name__ == "__main__":
+    SUPPORTED_TASKS["my-task"] = {
+        "impl": MyPipeline,
+        "tf": (),
+        "pt": (AutoModelForCausalLM,),
+        "default": {},
+        "type": "text",
+    }
 
-    generator = transformers.pipeline(task="fill-mask", model="julien-c/dummy-unknown")
-    print(generator("Today is a <mask> day"))
+    # tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
+    # model = AutoModelForCausalLM.from_pretrained("distilgpt2")
+    # generator = transformers.pipeline(task="my-task", model=model, tokenizer=tokenizer)
+    # print(generator("Gibraltar is a British Overseas Territory located at the southern tip of the Iberian Peninsula."))
 
-    # Save model to BentoML local model store
-    saved_model = bentoml.transformers.save_model("fill-mask", generator)
-    print(f"Model saved: {saved_model}")
+    # # Save model to BentoML local model store
+    # saved_model = bentoml.transformers.save_model("my-task", generator)
+    # print(f"Model saved: {saved_model}")
+
+    generator = transformers.pipeline(task="my-task", model="/Users/ssheng/bentoml/models/my-task/dofzizxtusakscvj")
+    print(generator("Gibraltar is a British Overseas Territory located at the southern tip of the Iberian Peninsula."))
