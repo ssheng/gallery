f08a27b96ae99b55f5ac897befaa434a519484b4
Sean Sheng <s3sheng@gmail.com>
2022-07-01 14:50:29 -0700

    Update transformers quickstart

--- quickstart/service.py
+++ quickstart/service.py
@@ -1,4 +1,5 @@
 import bentoml
+import numpy as np
 
 from bentoml.io import Text, NumpyNdarray
 
@@ -7,5 +8,5 @@ runner = bentoml.transformers.get("my-classification-task:latest").to_runner()
 svc = bentoml.Service("my-classification-service", runners=[runner])
 
 @svc.api(input=Text(), output=NumpyNdarray())
-def classify(input_series: str) -> dict:
+def classify(input_series: str) -> np.ndarray:
     return runner.run(input_series)
--- quickstart/train.py
+++ quickstart/train.py
@@ -26,35 +26,34 @@ class MyPipeline(Pipeline):
         return model_outputs["logits"].softmax(-1).numpy()
 
 if __name__ == "__main__":
-    # TASK_NAME = "my-classification-task"
-    # TASK_DEFINITION = {
-    #     "impl": MyPipeline,
-    #     "tf": (),
-    #     "pt": (AutoModelForSequenceClassification,),
-    #     "default": {},
-    #     "type": "text",
-    # }
-    # SUPPORTED_TASKS[TASK_NAME] = TASK_DEFINITION
+    TASK_NAME = "my-classification-task"
+    TASK_DEFINITION = {
+        "impl": MyPipeline,
+        "tf": (),
+        "pt": (AutoModelForSequenceClassification,),
+        "default": {},
+        "type": "text",
+    }
+    SUPPORTED_TASKS[TASK_NAME] = TASK_DEFINITION
 
-    # tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
-    # model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
-    # classifier = transformers.pipeline(task=TASK_NAME, model=model, tokenizer=tokenizer)
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+    model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+    classifier = transformers.pipeline(task=TASK_NAME, model=model, tokenizer=tokenizer)
 
-    # # Save model to BentoML local model store
-    # saved_model = bentoml.transformers.save_model(TASK_NAME, pipeline=classifier, task_name=TASK_NAME, task_definition=TASK_DEFINITION)
-    # print(f"Model saved: {saved_model}")
-    # print(saved_model("Gibraltar is a British Overseas Territory located at the southern tip of the Iberian Peninsula."))
+    # Save model to BentoML local model store
+    saved_model = bentoml.transformers.save_model(TASK_NAME, pipeline=classifier, task_name=TASK_NAME, task_definition=TASK_DEFINITION)
+    print(f"Model saved: {saved_model}")
 
     # generator = transformers.pipeline(task=TASK_NAME, model=f"/Users/ssheng/bentoml/models/{TASK_NAME}/6rkiutxug6e2qcvj")
     # print(generator.task)
     # print(generator("Gibraltar is a British Overseas Territory located at the southern tip of the Iberian Peninsula."))
 
-    pipeline = bentoml.transformers.load_model("my-classification-task:latest")
-    result = pipeline([
-        "BentoML: Create an ML Powered Prediction Service in Minutes via @TDataScience https://buff.ly/3srhTw9 #Python #MachineLearning #BentoML",
-        "Top MLOps Serving frameworks — 2021 https://link.medium.com/5Elq6Aw52ib #mlops #TritonInferenceServer #opensource #nvidia #machincelearning  #serving #tensorflow #PyTorch #Bodywork #BentoML #KFServing #kubeflow #Cortex #Seldon #Sagify #Syndicai",
-        "#MLFlow provides components for experimentation management, ML project management. #BentoML only focuses on serving and deploying trained models",
-        "2000 and beyond #OpenSource #bentoml",
-        "Model Serving Made Easy https://github.com/bentoml/BentoML ⭐ 1.1K #Python #Bentoml #BentoML #Modelserving #Modeldeployment #Modelmanagement #Mlplatform #Mlinfrastructure #Ml #Ai #Machinelearning #Awssagemaker #Awslambda #Azureml #Mlops #Aiops #Machinelearningoperations #Turn",
-    ])
-    print(result)
+    # pipeline = bentoml.transformers.load_model("my-classification-task:latest")
+    # result = pipeline([
+    #     "BentoML: Create an ML Powered Prediction Service in Minutes via @TDataScience https://buff.ly/3srhTw9 #Python #MachineLearning #BentoML",
+    #     "Top MLOps Serving frameworks — 2021 https://link.medium.com/5Elq6Aw52ib #mlops #TritonInferenceServer #opensource #nvidia #machincelearning  #serving #tensorflow #PyTorch #Bodywork #BentoML #KFServing #kubeflow #Cortex #Seldon #Sagify #Syndicai",
+    #     "#MLFlow provides components for experimentation management, ML project management. #BentoML only focuses on serving and deploying trained models",
+    #     "2000 and beyond #OpenSource #bentoml",
+    #     "Model Serving Made Easy https://github.com/bentoml/BentoML ⭐ 1.1K #Python #Bentoml #BentoML #Modelserving #Modeldeployment #Modelmanagement #Mlplatform #Mlinfrastructure #Ml #Ai #Machinelearning #Awssagemaker #Awslambda #Azureml #Mlops #Aiops #Machinelearningoperations #Turn",
+    # ])
+    # print(result)
