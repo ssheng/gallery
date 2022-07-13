8ec4e71d5690087c0a25d4a20f8c1a5b21beb67b
Sean Sheng <s3sheng@gmail.com>
2022-07-12 18:36:24 -0700

    Transformers quickstart

--- quickstart/service.py
+++ quickstart/service.py
@@ -1,12 +1,55 @@
+# import bentoml
+
+# from bentoml.io import Text, JSON
+
+# runner = bentoml.transformers.get("unmasker:latest").to_runner()
+
+# svc = bentoml.Service("unmasker_service", runners=[runner])
+
+# @svc.api(input=Text(), output=JSON())
+# def unmask(input_series: str) -> list:
+#     return runner.run(input_series)
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# import bentoml
+
+# from bentoml.io import Text, JSON
+# from transformers import pipeline
+
+
+# class PretrainedModelRunnable(bentoml.Runnable):
+#     SUPPORTED_RESOURCES = ()
+#     SUPPORTS_CPU_MULTI_THREADING = False
+
+#     def __init__(self):
+#         self.unmasker = pipeline(task="fill-mask", model="distilbert-base-uncased")
+
+#     @bentoml.Runnable.method(batchable=False)
+#     def __call__(self, input_text):
+#         return self.unmasker(input_text)
+
+# runner = bentoml.Runner(PretrainedModelRunnable, name="pretrained_unmasker")
+
+# svc = bentoml.Service('pretrained_unmasker_service', runners=[runner])
+
+# @svc.api(input=Text(), output=JSON())
+# def unmask(input_series: str) -> list:
+#     return runner.run(input_series)
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 import bentoml
-import numpy as np
 
-from bentoml.io import Text, NumpyNdarray
+from bentoml.io import Text, JSON
 
-runner = bentoml.transformers.get("my-classification-task:latest").to_runner()
+runner = bentoml.transformers.get("my_classification_model:latest").to_runner()
 
-svc = bentoml.Service("my-classification-service", runners=[runner])
+svc = bentoml.Service("my_classification_service", runners=[runner])
 
-@svc.api(input=Text(), output=NumpyNdarray())
-def classify(input_series: str) -> np.ndarray:
+@svc.api(input=Text(), output=JSON())
+def classify(input_series: str) -> list:
+    """
+    Classify the input text using the pretrained model.
+    """
     return runner.run(input_series)
--- quickstart/train.py
+++ quickstart/train.py
@@ -1,13 +1,6 @@
-import bentoml
-import transformers
-import logging
+from transformers import Pipeline
 
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSequenceClassification, Pipeline, PreTrainedTokenizer
-from transformers.pipelines import SUPPORTED_TASKS
-
-logging.basicConfig(level=logging.INFO)
-
-class MyPipeline(Pipeline):
+class MyClassificationPipeline(Pipeline):
     def _sanitize_parameters(self, **kwargs):
         preprocess_kwargs = {}
         if "maybe_arg" in kwargs:
@@ -15,7 +8,7 @@ class MyPipeline(Pipeline):
         return preprocess_kwargs, {}, {}
 
     def preprocess(self, text, maybe_arg=2):
-        input_ids = self.tokenizer(text, return_tensors='pt')
+        input_ids = self.tokenizer(text, return_tensors="pt")
         return input_ids
 
     def _forward(self, model_inputs):
@@ -25,35 +18,36 @@ class MyPipeline(Pipeline):
     def postprocess(self, model_outputs):
         return model_outputs["logits"].softmax(-1).numpy()
 
-if __name__ == "__main__":
-    TASK_NAME = "my-classification-task"
-    TASK_DEFINITION = {
-        "impl": MyPipeline,
-        "tf": (),
-        "pt": (AutoModelForSequenceClassification,),
-        "default": {},
-        "type": "text",
-    }
-    SUPPORTED_TASKS[TASK_NAME] = TASK_DEFINITION
+from transformers import pipeline
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from transformers.pipelines import SUPPORTED_TASKS
 
-    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
-    model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
-    classifier = transformers.pipeline(task=TASK_NAME, model=model, tokenizer=tokenizer)
+TASK_NAME = "my-classification-task"
+TASK_DEFINITION = {
+    "impl": MyClassificationPipeline,
+    "tf": (),
+    "pt": (AutoModelForSequenceClassification,),
+    "default": {},
+    "type": "text",
+}
+SUPPORTED_TASKS[TASK_NAME] = TASK_DEFINITION
 
-    # Save model to BentoML local model store
-    saved_model = bentoml.transformers.save_model(TASK_NAME, pipeline=classifier, task_name=TASK_NAME, task_definition=TASK_DEFINITION)
-    print(f"Model saved: {saved_model}")
+classifier = pipeline(
+    task=TASK_NAME,
+    model=AutoModelForSequenceClassification.from_pretrained(
+        "distilbert-base-uncased-finetuned-sst-2-english"
+    ),
+    tokenizer=AutoTokenizer.from_pretrained(
+        "distilbert-base-uncased-finetuned-sst-2-english"
+    ),
+)
 
-    # generator = transformers.pipeline(task=TASK_NAME, model=f"/Users/ssheng/bentoml/models/{TASK_NAME}/6rkiutxug6e2qcvj")
-    # print(generator.task)
-    # print(generator("Gibraltar is a British Overseas Territory located at the southern tip of the Iberian Peninsula."))
+import bentoml
 
-    # pipeline = bentoml.transformers.load_model("my-classification-task:latest")
-    # result = pipeline([
-    #     "BentoML: Create an ML Powered Prediction Service in Minutes via @TDataScience https://buff.ly/3srhTw9 #Python #MachineLearning #BentoML",
-    #     "Top MLOps Serving frameworks — 2021 https://link.medium.com/5Elq6Aw52ib #mlops #TritonInferenceServer #opensource #nvidia #machincelearning  #serving #tensorflow #PyTorch #Bodywork #BentoML #KFServing #kubeflow #Cortex #Seldon #Sagify #Syndicai",
-    #     "#MLFlow provides components for experimentation management, ML project management. #BentoML only focuses on serving and deploying trained models",
-    #     "2000 and beyond #OpenSource #bentoml",
-    #     "Model Serving Made Easy https://github.com/bentoml/BentoML ⭐ 1.1K #Python #Bentoml #BentoML #Modelserving #Modeldeployment #Modelmanagement #Mlplatform #Mlinfrastructure #Ml #Ai #Machinelearning #Awssagemaker #Awslambda #Azureml #Mlops #Aiops #Machinelearningoperations #Turn",
-    # ])
-    # print(result)
+bentoml.transformers.save_model(
+    "my_classification_model",
+    pipeline=classifier,
+    task_name=TASK_NAME,
+    task_definition=TASK_DEFINITION,
+)
