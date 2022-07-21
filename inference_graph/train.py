67508782392a90ec3a8a4b563e76028414e239aa
Sean Sheng <s3sheng@gmail.com>
2022-07-20 17:08:32 -0700

    tiny models

--- inference_graph/train.py
+++ inference_graph/train.py
@@ -7,13 +7,23 @@ logging.basicConfig(level=logging.WARN)
 
 if __name__ == "__main__":
     # Create Transformers pipelines from pretrained models
-    pipeline1 = transformers.pipeline(task="text-classification", model="bert-base-uncased", tokenizer="bert-base-uncased")
-    pipeline2 = transformers.pipeline(task="text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
-    pipeline3 = transformers.pipeline(task="text-classification", model="ProsusAI/finbert")
+    # pipeline1 = transformers.pipeline(task="text-classification", model="bert-base-uncased", tokenizer="bert-base-uncased")
+    # pipeline2 = transformers.pipeline(task="text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
+    # pipeline3 = transformers.pipeline(task="text-classification", model="ProsusAI/finbert")
+    
+    tiny_text_model = "hf-internal-testing/tiny-random-distilbert"
+    tiny_text_task = transformers.pipelines.get_task(tiny_text_model)
+    pipe = transformers.pipeline(task=tiny_text_task, moodel="tiny_text_model")
+
+
 
     # Save models to BentoML local model store
-    m1 = bentoml.transformers.save_model("bert-base-uncased", pipeline1)
-    m2 = bentoml.transformers.save_model("distilbert", pipeline2)
-    m3 = bentoml.transformers.save_model("prosusai-finbert", pipeline3)
+    # m1 = bentoml.transformers.save_model("bert-base-uncased", pipeline1)
+    # m2 = bentoml.transformers.save_model("distilbert", pipeline2)
+    # m3 = bentoml.transformers.save_model("prosusai-finbert", pipeline3)
+    model = bentoml.transformers.save_model("prosusai-finbert", pipe)
 
-    print(f"Model saved: {m1}, {m2}, {m3}")
+
+    # print(f"Model saved: {m1}, {m2}, {m3}")
+    print(f"Model saved: {model}")
+    ...
